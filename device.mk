cbb1c232103649972a176a9ca8e0d4d96750d866
rikka <ayasa0520@gmail.com>
2025-10-23 04:32:53 +0800

    Add virt wifi

--- device.mk
+++ device.mk
@@ -237,3 +237,27 @@ endif
 # Updater
 PRODUCT_PACKAGES += \
     WaydroidUpdater
+
+# WiFi: vendor side
+PRODUCT_SOONG_NAMESPACES += \
+	device/generic/goldfish 
+PRODUCT_PACKAGES += \
+	mac80211_create_radios \
+	createns \
+    dhcpclient \
+	dhcpserver \
+	execns \
+	hostapd \
+	hostapd_noaidl \
+	wpa_supplicant \
+	iw_vendor 
+
+PRODUCT_COPY_FILES += \
+    device/generic/goldfish/wifi/init.wifi.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.wifi.sh \
+    device/generic/goldfish/wifi/simulated_hostapd.conf:$(TARGET_COPY_OUT_VENDOR)/etc/simulated_hostapd.conf \
+    device/generic/goldfish/wifi/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf \
+    device/generic/goldfish/wifi/WifiConfigStore.xml:data/misc/wifi/WifiConfigStore.xml \
+    $(LOCAL_PATH)/init.net.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.net.rc \
+    frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
